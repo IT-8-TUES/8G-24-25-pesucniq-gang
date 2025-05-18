3891b064728ae1a22d2aa9601dea5ea83b5646db
ivanex183 <147521589+ivanex183@users.noreply.github.com>
2025-05-18 13:56:13 +0300

    moite gluposti(how to play i home page)

---
+++ website/app.js
@@ -0,0 +1,11 @@
+
+  function setBackground() {
+    document.body.style.backgroundImage = "url('image.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+  }
+
+
+  window.onload = setBackground;
+
