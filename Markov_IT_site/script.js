9bec10188d019c1a9229b264340b4888322791ed
B0jidaR10 <bojidarmarkov2708@gmail.com>
2025-05-18 22:01:50 +0300

    na markov

---
+++ Markov_IT_site/script.js
@@ -0,0 +1,4 @@
+function redirectToHomePage(event) {
+  event.preventDefault(); // Prevent form from submitting the default way
+  window.location.href = "HomePage.html"; // Redirect to the homepage
+}
